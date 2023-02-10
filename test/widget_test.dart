cd8b4b34daefd61525be517174d3a153a5166bb6
Odilbek <odilbekjuraev07@gmail.com>
2023-02-10 17:43:01 +0500

    some changes done

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -2,42 +2,39 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:unit_test_tutorial/util_model.dart';
 
 void main() {
-  Utils myUtils = Utils();
-
   group("Value testing passed", () {
-    test("myNumber's initial value equals to 100", () {
+    test("myNumber's initial value", () {
+      Utils myUtils = Utils();
       expect(myUtils.myNumber, 100);
     });
 
-    test("Base Url is Not Empty", () {
+    test("Base Url is Empty", () {
+      Utils myUtils = Utils();
       expect(myUtils.baseUrl.isEmpty, false);
     });
   });
 
   group("Methods testing", () {
-
     test("isMaxNew method testing", () {
-        expect(myUtils.isMaxNew(50, 10, 0, false), true);
+      Utils myUtils = Utils();
+      expect(myUtils.isMaxNew(50, 10, 0, false), true);
     });
 
     test("string equality testing", () {
+      Utils myUtils = Utils();
       expect(myUtils.stringEquals("Odil bek", "Odilbek"), true);
     });
 
-    group('Group test formatting', () {
-      test("Test formatting", () {
-        expect(Utils.format(1), "01");
-      });
-      test('Test formatting 2', (){
-        expect(Utils.format(11), "11");
-      });
-      test('Test formatting 3', (){
-        expect(Utils.format(3), "03");
-      });
+    test("Test formatting", () {
+      expect(Utils.format(1), "01");
+      expect(Utils.format(11), "11");
+      expect(Utils.format(3), "03");
     });
+
     test(
       "Card formatting test",
       () {
+        Utils myUtils = Utils();
         expect(myUtils.returnCardFormatText("8600312997956567"),
             "8600 3129 9795 6567");
       },
